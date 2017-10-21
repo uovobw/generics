2e889850b0669c106d7f5f4063beb85b95fadff1
Andrea Lusuardi - uovobw <uovobw@gmail.com>
2017-10-22 01:59:09 +0200

    chapters 10 11 12

---
+++ minigrep/src/main.rs
@@ -0,0 +1,28 @@
+extern crate minigrep;
+
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+//    let config = match Config::new(&args) {
+//        Ok(c) => c,
+//        Err(e) => {
+//            println!("Error parsing arguments: {}", e);
+//            process::exit(1);
+//        }
+//    };
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Error parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("FATAL: {}", e);
+        process::exit(1);
+    }
+}
+
